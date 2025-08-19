616d5e7ec4ddede9401fa713d307f458356c38fc
Mart Barbon <martbarbon8@gmail.com>
2025-08-19 22:13:24 +0800

    Activity 1
    >Practice 1 (Arrays)
    >Practice 2 (Linked List)

---
+++ Practice/practice1.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+#define MAX 10
+
+typedef struct{
+    char elem[MAX-1];
+    int count;
+}charList;
+
+bool findElem(charList, char); // Answer to #1
+
+void main(){
+
+    // Answer to #2 and #5
+    charList L;
+
+    L.elem[0] =  'U';
+    L.elem[1] =  'S';
+    L.elem[2] =  'C';
+    L.count = 3;
+
+    if(L.count > 1){
+        bool result = findElem(L,'D');
+
+        if(result){
+            printf("The list is not empty and element X is in the list.");
+        } else{
+            printf("The list is not empty and element X is not in the list.");
+        }
+    } else{
+        printf("The list is empty.");
+    }
+    
+}
+
+// Answer to #4
+bool findElem(charList L, char x){
+
+    int i;
+    for(i = 0; i < L.count && L.elem[i] != x; i++){}
+
+    return (i < L.count) ? true : false; 
+}
+
+// #3, draw an execution stack
